60e611c804b908c203518bbee64f8023c8f1fc3a
Morzh777 <103659109+Morzh777@users.noreply.github.com>
2022-06-17 22:55:31 +0300

    Добавляет Модуляцию

--- js/main.js
+++ js/main.js
@@ -1,21 +1,2 @@
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function getMaxLengthLine(LineLength, maxLength) {
-  let maxLengthLine;
-  if (LineLength <= maxLength) {
-    maxLengthLine = true;
-  } else {
-    maxLengthLine = false;
-  }
-  return maxLengthLine;
-}
-
-const randomNumber = getRandomNumber(1, 10);
-console.log(randomNumber);
-
-const MaxLengthLine =getMaxLengthLine(12,10);
-console.log(MaxLengthLine);
-
-
+import {addUserPhotos,} from './data.js';
+console.log(addUserPhotos());
