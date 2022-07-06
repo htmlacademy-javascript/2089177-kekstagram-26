a8b9c14424b1633d91c9b01547f3c12016a7570f
Keks <keks@htmlacademy.ru>
2022-07-06 20:43:10 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/display-photo.js
@@ -0,0 +1,66 @@
+import { bigPictureExit } from './util.js';
+
+const bigPicture = document.querySelector('.big-picture');
+const body = document.querySelector('body');
+const bigPictureCancel = document.querySelector('.big-picture__cancel');
+
+
+// скрываем лишнее
+const commentsCount = bigPicture.querySelector('.social__comment-count');
+const commentsLoader = bigPicture.querySelector('.comments-loader');
+commentsCount.classList.add('hidden');
+commentsLoader.classList.add('hidden');
+
+const bigPictureOff = () => {
+  bigPicture.classList.add('hidden');
+  body.classList.remove('modal-open');
+  bigPictureCancel.removeEventListener('click', bigPictureOff);
+};
+
+const bigPictureOffEscape = () => {
+  bigPicture.classList.add('hidden');
+  body.classList.remove('modal-open');
+  bigPictureCancel.removeEventListener('keydown', bigPictureOffEscape);
+};
+
+
+// функция вывода комментариев
+const commentTemplate = document.querySelector('.social__comment');
+const commentList = document.querySelector('.social__comments');
+// рендерю комент
+const renderComment = (comment) => {
+  const commentSimilar = commentTemplate.cloneNode(true);
+
+  commentSimilar.querySelector('.social__picture').src = comment.avatar;
+  commentSimilar.querySelector('.social__picture').alt = comment.name;
+  commentSimilar.querySelector('.social__text').textContent = comment.message;
+
+  return commentSimilar;
+};
+// рендерю коменты
+const renderComments = (comments) => {
+  const commentsListFragment = document.createDocumentFragment();
+
+  comments.forEach((comment) => {
+    commentsListFragment.appendChild(renderComment(comment));
+  });
+
+  commentList.appendChild(commentsListFragment);
+};
+
+// Отображение Попапа большой картинки
+const showPicture = (picture) => {
+  commentList.innerHTML = '';
+  body.classList.add('modal-open');
+  bigPicture.querySelector('.big-picture__img > img').src = picture.url;
+  bigPicture.querySelector('.likes-count').textContent = picture.likes;
+  bigPicture.querySelector('.comments-count').textContent =picture.comments.length;
+  bigPicture.querySelector('.social__caption').textContent =picture.description;
+  bigPictureCancel.addEventListener('click', bigPictureOff);
+  bigPictureExit('keydown');
+  renderComments(picture.comments);
+  bigPicture.classList.remove('hidden');
+
+};
+
+export { showPicture };
---
+++ 7/js/util.js
@@ -0,0 +1,22 @@
+// Функция по поиску рандомного значения
+function getRandomNumber(first, last) {
+  const firstValue = Math.ceil(Math.min(Math.abs(first), Math.abs(last)));
+  const lastValue = Math.floor(Math.max(Math.abs(first), Math.abs(last)));
+  return Math.floor(Math.random() * (lastValue - firstValue + 1) + firstValue);
+}
+// Функция по возврату случайного элемента в массиве
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+// Проверка максимальной длинны строки
+function getMaxStringLength(string, length) {
+  return string.length <= length;
+}
+// Закрываем попап на esc
+const bigPictureExit =() => {document.body.addEventListener('keydown',  (evt) => {
+  if(evt.keyCode === 27) {
+    document.body.classList.remove('modal-open');
+    document.querySelector('.big-picture').classList.add('hidden');
+  }
+});
+};
+export { getRandomNumber,getRandomArrayElement,bigPictureExit};
+
