4285c4f2d0990575fba502a483b6b53615dc9cc1
Keks <keks@htmlacademy.ru>
2022-07-19 20:38:37 +0300

    :heavy_check_mark: Сборка #15

---
+++ 15/js/display-photo.js
@@ -0,0 +1,83 @@
+import { isEscapeKey } from './util.js';
+const COMMENTS_LOAD_STEP =5;
+
+const bigPicture = document.querySelector('.big-picture');
+const body = document.querySelector('body');
+const bigPictureClose = document.querySelector('.big-picture__cancel');
+const commentList = document.querySelector('.social__comments');
+const commentTemplate = commentList.querySelector('.social__comment');
+
+
+let commentsLoaded = [];
+
+const commentCount = bigPicture.querySelector('.social__comment-count');
+const commentsLoader = bigPicture.querySelector('.comments-loader');
+
+let commentsCount = COMMENTS_LOAD_STEP;
+
+
+// рендерю комент
+const renderComment = (comment) => {
+  const commentSimilar = commentTemplate.cloneNode(true);
+
+  commentSimilar.querySelector('.social__picture').src = comment.avatar;
+  commentSimilar.querySelector('.social__picture').alt = comment.name;
+  commentSimilar.querySelector('.social__text').textContent = comment.message;
+
+  return commentSimilar;
+};
+
+// рендерю коменты
+const renderComments = (comments) => {
+  commentsCount = (comments.length < COMMENTS_LOAD_STEP) ? comments.length : commentsCount;
+  commentsLoaded = comments.slice(0, commentsCount);
+  commentList.innerHTML = '';
+  commentCount.textContent = `${commentsLoaded.length} из ${comments.length} комментариев`;
+  const commentsListFragment = document.createDocumentFragment();
+  commentsLoaded.forEach((comment) => {
+    commentsListFragment.appendChild(renderComment(comment));
+  });
+  commentList.appendChild(commentsListFragment);
+  if (comments.length > COMMENTS_LOAD_STEP && commentsLoaded.length < comments.length) {
+    commentsLoader.classList.remove('hidden');
+  } else {
+    commentsLoader.classList.add('hidden');
+  }
+  commentsCount += COMMENTS_LOAD_STEP;
+};
+
+// Отображение Попапа большой картинки
+const showPicture = (picture) => {
+  renderComments(picture.comments.slice());
+
+  const onCommentsLoaderClick = () => {
+    renderComments(picture.comments.slice());
+  };
+  body.classList.add('modal-open');
+  bigPicture.querySelector('.big-picture__img > img').src = picture.url;
+  bigPicture.querySelector('.likes-count').textContent = picture.likes;
+  bigPicture.querySelector('.social__caption').textContent =picture.description;
+
+  bigPicture.classList.remove('hidden');
+
+  const closeModalDisplayPhoto = (evt) => {
+    bigPicture.classList.add('hidden');
+    body.classList.remove('modal-open');
+    bigPictureClose.removeEventListener('click', closeModalDisplayPhoto);
+    commentList.innerHTML = '';
+    commentsCount = COMMENTS_LOAD_STEP;
+    commentsLoaded = [];
+    commentsLoader.removeEventListener('click', onCommentsLoaderClick);
+    body.removeEventListener('keydown', closeModalDisplayPhoto);
+    if (isEscapeKey(evt)){
+      closeModalDisplayPhoto();
+    }
+  };
+
+  commentsLoader.addEventListener('click', onCommentsLoaderClick);
+  bigPictureClose.addEventListener('click', closeModalDisplayPhoto);
+  body.addEventListener('keydown', closeModalDisplayPhoto);
+
+};
+
+export { showPicture };
---
+++ 15/js/main.js
@@ -0,0 +1,67 @@
+
+import { renderPhotos } from './preview-photos.js';
+import { request } from './fetch.js';
+import { showError } from './alerts.js';
+import { debounce,sortArray } from './util.js';
+import './editor-photo.js';
+import './validation.js';
+import './load-photo.js';
+
+const DEFAULT_PREVIEW_LOAD = 25;
+const RANDOM_PREVIEW_LOAD = 10;
+const DEBOUNCE_INTERVAL = 300;
+
+const filter = document.querySelector('.img-filters');
+
+let photos = [];
+
+const  removeActiveClass = () => {
+  const activeFilter = document.querySelector('.img-filters__button--active');
+  activeFilter.classList.remove('img-filters__button--active');
+};
+
+const removePhotos = () => {
+  const images = document.querySelectorAll('.picture');
+  if (images) {
+    images.forEach((element) => {
+      element.remove();
+    });
+  }
+};
+
+const filters = {
+  'filter-default': () => {
+    renderPhotos(photos.slice(0, DEFAULT_PREVIEW_LOAD));
+  },
+  'filter-random': () => {
+    renderPhotos(sortArray(photos.slice()).slice(0, RANDOM_PREVIEW_LOAD));
+
+  },
+  'filter-discussed': () => {
+    renderPhotos(photos.slice().sort((a, b) => b.comments.length - a.comments.length));
+  },
+};
+
+
+const onSuccess = (data) => {
+  filter.classList.remove('img-filters--inactive');
+  photos = data.slice();
+  renderPhotos(data.slice(0,DEFAULT_PREVIEW_LOAD));
+};
+
+const onError = () => {
+  showError('Ошибка загрузки!', 'Закрыть');
+};
+
+request(onSuccess, onError, 'GET');
+
+const onFilterClick = debounce((evt) => {
+  if (evt.target.classList.contains('img-filters__button')) {
+    removeActiveClass();
+    removePhotos();
+    evt.target.classList.add('img-filters__button--active');
+    filters[evt.target.id]();
+  }
+},DEBOUNCE_INTERVAL);
+
+filter.addEventListener('click', onFilterClick);
