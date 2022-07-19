7d4966d6aed4c3f7af8f24a461a48a410a124556
Morzh777 <103659109+Morzh777@users.noreply.github.com>
2022-07-19 19:39:37 +0300

    12.11. Перламутровые пуговицы

--- js/display-photo.js
+++ js/display-photo.js
@@ -1,4 +1,4 @@
-
+import { isEscapeKey } from './util.js';
 const COMMENTS_LOAD_STEP =5;
 
 const bigPicture = document.querySelector('.big-picture');
@@ -15,14 +15,6 @@ const commentsLoader = bigPicture.querySelector('.comments-loader');
 
 let commentsCount = COMMENTS_LOAD_STEP;
 
-const closeModalDisplayPhoto = () => {
-  bigPicture.classList.add('hidden');
-  body.classList.remove('modal-open');
-  bigPictureClose.removeEventListener('click', closeModalDisplayPhoto);
-  commentList.innerHTML = '';
-  commentsCount = COMMENTS_LOAD_STEP;
-  commentsLoaded = [];
-};
 
 // рендерю комент
 const renderComment = (comment) => {
@@ -37,48 +29,56 @@ const renderComment = (comment) => {
 
 // рендерю коменты
 const renderComments = (comments) => {
-
-  const onCommentsLoaderClick = () => {
-    renderComments(comments);
-  };
-
   commentsCount = (comments.length < COMMENTS_LOAD_STEP) ? comments.length : commentsCount;
   commentsLoaded = comments.slice(0, commentsCount);
-
   commentList.innerHTML = '';
-
   commentCount.textContent = `${commentsLoaded.length} из ${comments.length} комментариев`;
-
   const commentsListFragment = document.createDocumentFragment();
-
   commentsLoaded.forEach((comment) => {
     commentsListFragment.appendChild(renderComment(comment));
   });
-
   commentList.appendChild(commentsListFragment);
-
   if (comments.length > COMMENTS_LOAD_STEP && commentsLoaded.length < comments.length) {
     commentsLoader.classList.remove('hidden');
-    commentsLoader.addEventListener('click', onCommentsLoaderClick, { once: true });
   } else {
     commentsLoader.classList.add('hidden');
   }
-
   commentsCount += COMMENTS_LOAD_STEP;
 };
 
 // Отображение Попапа большой картинки
 const showPicture = (picture) => {
-  commentsCount = COMMENTS_LOAD_STEP;
-  commentsLoaded = [];
+
+  const onCommentsLoaderClick = () => {
+    renderComments(picture.comments.slice());
+  };
   body.classList.add('modal-open');
   bigPicture.querySelector('.big-picture__img > img').src = picture.url;
   bigPicture.querySelector('.likes-count').textContent = picture.likes;
   bigPicture.querySelector('.social__caption').textContent =picture.description;
-  bigPictureClose.addEventListener('click', closeModalDisplayPhoto);
+
   bigPicture.classList.remove('hidden');
 
   renderComments(picture.comments.slice());
+
+  const closeModalDisplayPhoto = (evt) => {
+    bigPicture.classList.add('hidden');
+    body.classList.remove('modal-open');
+    bigPictureClose.removeEventListener('click', closeModalDisplayPhoto);
+    commentList.innerHTML = '';
+    commentsCount = COMMENTS_LOAD_STEP;
+    commentsLoaded = [];
+    commentsLoader.removeEventListener('click', onCommentsLoaderClick);
+    body.removeEventListener('keydown', closeModalDisplayPhoto);
+    if (isEscapeKey(evt)){
+      closeModalDisplayPhoto();
+    }
+  };
+
+  commentsLoader.addEventListener('click', onCommentsLoaderClick);
+  bigPictureClose.addEventListener('click', closeModalDisplayPhoto);
+  body.addEventListener('keydown', closeModalDisplayPhoto);
+
 };
 
 export { showPicture };
